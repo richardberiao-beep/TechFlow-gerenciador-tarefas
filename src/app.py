a0d99ae9e929c1fc2b1803ba916938203c6082ea
Richard <richardberiao@gmail.com>
2026-07-04 15:36:04 -0300

    feat: adiciona rota para listagem de tarefas

--- src/app.py
+++ src/app.py
@@ -10,3 +10,22 @@ id_atual = 1
 def criar_tarefa():
     global id_atual
     dados = request.get_json()
+
+    # Validação obrigatória exigida no desafio
+    if not dados or 'titulo' not in dados:
+        return jsonify({'erro': 'O titulo e obrigatorio'}), 400
+
+    nova_tarefa = {
+        'id': id_atual,
+        'titulo': dados['titulo'],
+        'descricao': dados.get('descricao', ''),
+        'status': dados.get('status', 'A Fazer'),
+        'prazo': dados.get('prazo', None) # Incluído para a mudança de escopo
+    }
+    tarefas.append(nova_tarefa)
+    id_atual += 1
+    return jsonify(nova_tarefa), 201
+
+@app.route('/tarefas', methods=['GET'])
+def listar_tarefas():
+    return jsonify(tarefas), 200
